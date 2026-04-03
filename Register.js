3f253c9f2fb10c7c0dc441b3f8722ca572b28dcd
DanielJeon1101 <hyunw0820@gmail.com>
2026-04-03 14:45:02 +0900

    123

--- Register.js
+++ Register.js
@@ -1,32 +1,30 @@
-// Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCYkG8uZUET5UnW80IHKGxhL7WpNCp7kfQ",
-    authDomain: "q-log-f6aed.firebaseapp.com",
-    projectId: "q-log-f6aed",
-    storageBucket: "q-log-f6aed.firebasestorage.app",
-    messagingSenderId: "373152234648",
-    appId: "1:373152234648:web:aa2d94664a5674aca9e4ed",
-    measurementId: "G-TWK7RMPLR6"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCYkG8uZUET5UnW80IHKGxhL7WpNCp7kfQ",
+  authDomain: "q-log-f6aed.firebaseapp.com",
+  projectId: "q-log-f6aed",
+  storageBucket: "q-log-f6aed.firebasestorage.app",
+  messagingSenderId: "373152234648",
+  appId: "1:373152234648:web:aa2d94664a5674aca9e4ed"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+document.getElementById('submit').addEventListener('click', function(event) {
+  event.preventDefault();
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
 
-  
-  const submit = document.getElementById('submit');
-  submit.addEventListener("click",function(event){
-    event.preventDefault()
-    alert(5)
-  }
-)
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      alert('가입이 완료되었습니다!');
+      location.href = 'index.html';
+    })
+    .catch((error) => {
+      alert('오류: ' + error.message);
+    });
+});
